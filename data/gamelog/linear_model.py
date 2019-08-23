e0c6a3124ff60d0a392f13ace767691d64d99375
hunshih <hunshih@umich.edu>
2019-08-22 23:09:57 -0400

    successful for 6 graph

--- data/gamelog/linear_model.py
+++ data/gamelog/linear_model.py
@@ -11,12 +11,14 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
 #graph linear model and return the accuracy and regression
-def linear(x_train, x_test, y_train, y_test):
+def linear(x_train, x_test, y_train, y_test, target):
     reg = LinearRegression().fit(x_train, y_train)
     yard_pred = reg.predict(x_test)
     
     # The coefficients
     print('Coefficients: \n', reg.coef_)
+
+    print('Score: ', reg.score(x_test, y_test))
 
     # The mean squared error
     print("Mean squared error: %.2f"
@@ -26,13 +28,16 @@ def linear(x_train, x_test, y_train, y_test):
     print('Variance score: %.2f' % r2_score(y_test, yard_pred))
 
     # Plot outputs
+    plt.figure()
+    plt.xlabel(target, fontsize=18)
+    plt.ylabel('yards', fontsize=16)
     plt.scatter(x_test, y_test,  color='black')
     plt.plot(x_test, yard_pred, color='blue', linewidth=3)
 
-    plt.xticks(())
-    plt.yticks(())
-
-    plt.show()
+def adjustedScore(sack, inter, tackle):
+    result = sack*1.2 + inter*1.5 + tackle
+    print result
+    return result
 
 # return (x_train data, y_train, x_test data, y_test)
 def getData(target):
@@ -45,31 +50,27 @@ def getData(target):
     y = []
     for row in rows:
         x_sub = []
+        #x_sub.append(adjustedScore(row[2], row[3], row[4]))
         x_sub.append(row[2])
         x.append(x_sub)
         y.append(row[1])
     return (x, y)
 
 # return accuracy
-def correlation(target, color):
+def correlation(target):
     #print getData(target)
     (x, y) = getData(target)
-    print x
-    print y
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
-    #plt.scatter(x_train, y_train)
-    #plt.title(target)
-    #plt.show()
-    print x_train
-    print x_test
-    linear(x_train, x_test, y_train, y_test)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=False)
+    linear(x_train, x_test, y_train, y_test, target)
 
 def main():
-    correlation("sacks", "bo")
-    '''correlation("intercep", "go")
-    correlation("tackles", "ro")
-    correlation("avg_sack", "bo")
-    correlation("avg_int", "go")
-    correlation("avg_tackle", "ro")'''
+    correlation("sacks")
+    correlation("intercep")
+    correlation("tackles")
+    correlation("avg_sack")
+    correlation("avg_int")
+    correlation("avg_tackle")
+    #correlation("avg_sack, avg_int, avg_tackle")
+    plt.show()
 
 main()
